d027317521b321a51edf8c54a68c72b4af68c491
film-eyes <nick.ved16@gmail.com>
2025-09-22 10:50:52 +0300

    google auth fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./contexts/AuthContext.jsx"; // 👈 добавили
+import { AuthProvider } from "./contexts/AuthContext.jsx"; // оставляем контекст
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
