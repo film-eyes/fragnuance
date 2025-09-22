f5e1d5ec81d4d33d006f73b5374a03634c2986ee
film-eyes <nick.ved16@gmail.com>
2025-09-22 13:44:42 +0300

    new layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,25 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen flex flex-col relative">
-      <Header />         {/* ← теперь всегда виден, в т.ч. на главной */}
-      <main className="flex-1">
+    <div className="relative min-h-screen">
+      {/* Хедер */}
+      <header className="fixed top-0 left-0 w-full z-50 bg-transparent">
+        <Header />
+      </header>
+
+      {/* Контент */}
+      <main className="min-h-screen">
         <Outlet />
       </main>
-      <Footer />
+
+      {/* Футер */}
+      <footer className="fixed bottom-0 left-0 w-full z-50 bg-transparent">
+        <Footer />
+      </footer>
     </div>
   );
 }
